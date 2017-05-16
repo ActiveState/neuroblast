115562e2655fb0ad5dd7fa5a714613d5c95946b3
Pete Garcin <peteg@activestate.com>
2017-05-15 20:45:18 -0700

    Windows launch command

---
+++ rungame.cmd
@@ -0,0 +1,9 @@
+@ECHO OFF
+IF EXIST data GOTO DATAEXIST
+mkdir data
+cd data
+mkdir db
+cd ..
+:DATAEXIST
+START "Mongo" "C:\Program Files\MongoDB\Server\3.4\bin\mongod" -dbpath data\db
+python game.py

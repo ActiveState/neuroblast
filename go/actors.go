d3b5c5c86d7250d21462f52c327a9ff8ddb2b27b
Pete Garcin <peteg@activestate.com>
2017-07-10 16:53:17 -0300

    Merging golang project

---
+++ go/actors.go
@@ -0,0 +1,70 @@
+package main
+
+import "github.com/faiface/pixel"
+import "time"
+
+type baseActor struct {
+	lives       int
+	health      int
+	blinking    bool
+	blinktime   float64
+	blinkcycles int
+	blinks      int
+	blinkon     bool
+	//Functioncallback
+
+}
+
+func (b *baseActor) TakeDamage(dmg int) {
+	if b.health < 0 {
+		return
+	}
+
+	b.health -= dmg
+
+	if b.health <= 0 {
+		b.lives--
+	}
+	//if b.deadcb:
+	//	self.deadcb()
+	if b.lives >= 0 {
+		// DO something
+		// Trigger Particle or something
+		b.blinking = true
+		b.blinks = 0
+		b.health = 100
+	}
+}
+
+/*	phys := &gopherPhys{
+	gravity:   -512,
+	runSpeed:  64,
+	jumpSpeed: 192,
+	rect:      pixel.R(-6, -7, 6, 7),
+}*/
+
+type bullet struct {
+	pos    pixel.Vec
+	vel    pixel.Vec
+	rect   pixel.Rect
+	sprite *pixel.Sprite
+}
+
+type actor struct {
+	hitAnim   *spriteAnim
+	blowAnim  *spriteAnim
+	idleAnim  *spriteAnim
+	cooldown  float64
+	canfire   bool
+	bulcount  float64
+	pos       pixel.Vec
+	vel       pixel.Vec
+	rect      pixel.Rect
+	hitSpot   pixel.Rect
+	spawnTime time.Time
+
+	health int
+	lives  int
+	// Function callbacks??? Channels?
+
+}
